8e5f081ef21ec34545dfcac89603902dd07295ba
Ajea Smith <smithajea@rocketmail.com>
2022-05-20 07:25:35 -0400

    basic setup / added launch page and route

---
+++ src/components/launch/launch-component.jsx
@@ -0,0 +1,47 @@
+import {
+	Button,
+	LaunchWrapper,
+	Wrapper,
+	Logo,
+	ForEmployees,
+	ForEmployers,
+	Text,
+	Header,
+} from './launch.styles';
+const Launch = () => {
+	return (
+		<Wrapper>
+			<Logo>
+				Hire <span style={{ color: '#f7578c' }}>+Plus</span>
+			</Logo>
+			<LaunchWrapper>
+				<ForEmployers>
+					<Header>For Employers</Header>
+					<Text>
+						Easily search and view candidates, manage candidates <br />{' '}
+						through our Project Management board, best of all post jobs
+						for free!
+					</Text>
+					<Button>
+						GET STARTED
+						<span style={{ marginLeft: '10px' }}>&#10095;</span>{' '}
+					</Button>
+				</ForEmployers>
+				<ForEmployees>
+					<Header>For Employees</Header>
+					<Text>
+						View the latest jobs posted by hiring companies, create and
+						manage your profile to show off skills, and apply for jobs you
+						want!{' '}
+					</Text>
+					<Button>
+						GET STARTED
+						<span style={{ marginLeft: '10px' }}>&#10095;</span>{' '}
+					</Button>
+				</ForEmployees>
+			</LaunchWrapper>
+		</Wrapper>
+	);
+};
+
+export default Launch;
